08c4366af1c4200cdcfa38b57fa484cade94d8c6
jodlo005 <jodlo005@umn.edu>
2023-04-03 20:53:16 -0500

    new

--- code.R
+++ code.R
@@ -1,33 +1,25 @@
 library(tidyverse)
 library(data.table) ## For the fread function
 library(lubridate)
+library(googledrive)
 
 source("sepsis_monitor_functions.R")
 
-df <- initializePatients()
-
-
-library(googledrive)
-
-# We have to write the file to disk first, then upload it
-df %>% write_csv("sepsis_report_temp.csv")
-
-# Uploading happens here
-drive_put(media = "sepsis_report_temp.csv",  
-          path = "https://drive.google.com/drive/u/1/folders/1iR5y6jUC2qjC7DTj9mNwMrzUaTOpPjgl",
-          name = "sepsis_report.csv")
-
-
+#reading file
 file_link <- "https://drive.google.com/file/d/1pH0zGLKzuQz9KqixikuPythYqgINWWob/view?usp=sharing"
 sepsis <- drive_read_string(file_link) %>%
   read_csv()
 
+#changing the name of the current file as the "sepsis_report.csv" file
 drive_put(media = "sepsis_report_up.csv",  
           path = "https://drive.google.com/drive/u/1/folders/1iR5y6jUC2qjC7DTj9mNwMrzUaTOpPjgl",
           name = "sepsis_report.csv")
+
+#updating patients
 up_sepsis<-updatePatients(sepsis)
 up_sepsis %>% write_csv("sepsis_report_up.csv")
 
+#uploading updated report
 drive_put(media = "sepsis_report_up.csv",  
           path = "https://drive.google.com/drive/u/1/folders/1iR5y6jUC2qjC7DTj9mNwMrzUaTOpPjgl",
           name = "sepsis_report_up.csv")
