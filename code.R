d6e34f7657e4e99561f2b78762534778b8d645c9
jodlo005 <jodlo005@umn.edu>
2023-04-05 22:14:49 -0500

    after alterations!

--- code.R
+++ code.R
@@ -3,30 +3,77 @@ library(data.table) ## For the fread function
 library(lubridate)
 library(ggplot2)
 library(googledrive)
+library(tictoc)
 
 source("sepsis_monitor_functions.R")
 
+df <- makeSepsisDataset()
+
+# We have to write the file to disk first, then upload it
+df %>% write_csv("sepsis_data_temp.csv")
+
+# Uploading happens here
+sepsis_file <- drive_put(media = "sepsis_data_temp.csv", 
+                         path = "https://drive.google.com/drive/u/1/folders/1iR5y6jUC2qjC7DTj9mNwMrzUaTOpPjgl",
+                         name = "sepsis_data.csv")
+
+# Set the file permissions so anyone can download this file.
+sepsis_file %>% drive_share_anyone()
+
 #reading file
 file_link <- "https://drive.google.com/file/d/1XfmwcKpyQrX3UH0yF2ht6VwbmxHMuj1h/view?usp=share_link"
-sepsis <- drive_read_string(file_link) %>%
+up_sepsis <- drive_read_string(file_link) %>%
   read_csv()
+up_sepsis %>% write_csv("sepsis_report_up.csv")
 
-#changing the name of the current file as the "sepsis_report.csv" file
+file_link<-"https://drive.google.com/file/d/1Rt8moMSEKvlsGTD8CM63mcFLpFR3o9TL/view?usp=share_link"
+new_data <- updateData(file_link)
+most_recent_data <- new_data %>%
+  group_by(PatientID) %>%
+  filter(obsTime == max(obsTime))
+
+#changing the name of the "sepsis_report_up.csv" to the "sepsis_report.csv" file
 drive_put(media = "sepsis_report_up.csv",  
           path = "https://drive.google.com/drive/u/1/folders/1iR5y6jUC2qjC7DTj9mNwMrzUaTOpPjgl",
           name = "sepsis_report.csv")
 
-#updating patients
-up_sepsis<-updatePatients(sepsis)
-up_sepsis %>% write_csv("sepsis_report_up.csv")
+most_recent_data %>% write_csv("sepsis_report_updated.csv")
 
 #uploading updated report
-drive_put(media = "sepsis_report_up.csv",  
+drive_put(media = "sepsis_report_updated.csv",  
           path = "https://drive.google.com/drive/u/1/folders/1iR5y6jUC2qjC7DTj9mNwMrzUaTOpPjgl",
           name = "sepsis_report_up.csv")
 
-for (i in ids[4]) {
-  b<-getPatient(i)
-  ggplot(data=a, aes(x=ICULOS, y=HR, group=1)) +
-    geom_line()+
-    geom_point()}
+#FREAD:
+
+tic()
+makeSepsisDataset()
+toc()
+#5.67s elapsed
+
+tic()
+makeSepsisDataset(n=100)
+toc()
+#10.39s elapsed
+
+tic()
+makeSepsisDataset(n=500)
+toc()
+#54.59s elapsed
+
+#read_delim:
+
+tic()
+makeSepsisDataset(read_fn="read_delim")
+toc()
+#24.26s elapsed
+
+tic()
+makeSepsisDataset(n=100, read_fn="read_delim")
+toc()
+#50.47s elapsed
+
+tic()
+makeSepsisDataset(n=500, read_fn="read_delim")
+toc()
+#249.33s elapsed
